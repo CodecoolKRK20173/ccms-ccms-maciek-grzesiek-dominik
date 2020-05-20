590a5e2ebe50bb53080c4bc43c1f2a5b6b453e2a
Dominik <domek99@gmail.com>
2020-05-20 15:02:04 +0200

    move addUser and remove methods from Dao to AdminDao

--- src/main/java/com/codecool/dao/AdminDao.java
+++ src/main/java/com/codecool/dao/AdminDao.java
@@ -1,25 +1,43 @@
 package com.codecool.dao;
 
-import com.codecool.IO;
 import com.codecool.models.User;
 
-import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class AdminDao extends Dao {
-    private final IO io;
 
     public AdminDao() {
-        io = new IO();
     }
 
-    private User createUser() {
-        String name = io.gatherInput("New user's name: ");
-        String surname = io.gatherInput("New user's surname: ");
-        String email = io.gatherInput("New user's email: ");
-        String password = io.gatherInput("New user's password: ");
-        String phoneNumber = io.gatherInput("New user's phone number: ");
-        int idRole = io.gatherIntInput("New user's role id(1 - Admin " +
-                "2 - Mentor 3 - Student 4 - Employee): ", 1, 4);
-        return new User(name, surname, email, password, phoneNumber, idRole);
+    public void addMentor(User user) {
+        connect();
+        PreparedStatement addUser;
+        String addString = "INSERT INTO Users (name, surname, email, password, PhoneNumber, RoleId) VALUES (?, ?, ?, ?, ?)";
+        try {
+            addUser = connection.prepareStatement(addString);
+            addUser.setString(1, user.getName());
+            addUser.setString(1, user.getSurname());
+            addUser.setString(2, user.getEmail());
+            addUser.setString(3, user.getPassword());
+            addUser.setString(4, user.getPhoneNumber());
+            addUser.setInt(5, user.getRole());
+            addUser.executeUpdate();
+            addUser.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void remove(String table, String id) {
+        String query = String.format("DELETE FROM %s WHERE Id = %s;", table, id);
+
+        connect();
+        try {
+            statement.execute(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/codecool/dao/Dao.java
+++ src/main/java/com/codecool/dao/Dao.java
@@ -41,35 +41,4 @@ public abstract class Dao {
         }
     }
 
-    public void addUser(User user) {
-        connect();
-        PreparedStatement addUser;
-        String addString = "INSERT INTO Users (name, surname, email, password, PhoneNumber, RoleId) VALUES (?, ?, ?, ?, ?)";
-        try {
-            addUser = connection.prepareStatement(addString);
-            addUser.setString(1, user.getName());
-            addUser.setString(1, user.getSurname());
-            addUser.setString(2, user.getEmail());
-            addUser.setString(3, user.getPassword());
-            addUser.setString(4, user.getPhoneNumber());
-            addUser.setInt(5, user.getRole());
-            addUser.executeUpdate();
-            addUser.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void remove(String table, String id) {
-        String query = String.format("DELETE FROM %s WHERE Id = %s;", table, id);
-
-        connect();
-        try {
-            statement.execute(query);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
