7849374550b81352facb46e36870e7dec0697510
Dominik <domek99@gmail.com>
2020-05-20 09:45:21 +0200

    add createUser method to create user from input

--- src/main/java/com/codecool/dao/AdminDao.java
+++ src/main/java/com/codecool/dao/AdminDao.java
@@ -1,4 +1,24 @@
 package com.codecool.dao;
 
-public class AdminDao {
+import com.codecool.IO;
+import com.codecool.models.User;
+
+public class AdminDao extends Dao {
+    private IO io;
+
+    AdminDao() {
+        io = new IO();
+    }
+
+    private User createUser() {
+        String name = io.gatherInput("New user's name: ");
+        String surname = io.gatherInput("New user's surname: ");
+        String email = io.gatherInput("New user's email: ");
+        String password = io.gatherInput("New user's password: ");
+        String phoneNumber = io.gatherInput("New user's phone number: ");
+        int idRole = io.gatherIntInput("New user's role id: ", 1, 4);
+        return new User(name, surname, email, password, phoneNumber, idRole);
+    }
+
+
 }
