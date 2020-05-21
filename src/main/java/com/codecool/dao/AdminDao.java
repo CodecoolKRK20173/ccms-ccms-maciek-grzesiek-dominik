870654b6bd5b946e82bae890c25c7804ac07ef30
Dominik <domek99@gmail.com>
2020-05-21 11:06:15 +0200

    delete methods not used in AdminDao

--- src/main/java/com/codecool/dao/AdminDao.java
+++ src/main/java/com/codecool/dao/AdminDao.java
@@ -10,22 +10,6 @@ public class AdminDao extends Dao {
     public AdminDao() {
     }
 
-//    private User createUser() {
-//        String name = io.gatherInput("New user's name: ");
-//        String surname = io.gatherInput("New user's surname: ");
-//        String email = io.gatherInput("New user's email: ");
-//        String password = io.gatherInput("New user's password: ");
-//        String phoneNumber = io.gatherInput("New user's phone number: ");
-//        int idRole = io.gatherIntInput("New user's role id(1 - Admin " +
-//                "2 - Mentor 3 - Student 4 - Employee): ", 1, 4);
-//        return new User(name, surname, email, password, phoneNumber, idRole);
-//    }
-
-//    public void addUserToDB() {
-//        User newUser = createUser();
-//        addUser(newUser);
-//    }
-
     public void addMentor(User user) {
         connect();
         PreparedStatement addUser;
